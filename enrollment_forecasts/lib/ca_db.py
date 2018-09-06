73cfd647bc6c1edcf4641b0545f06048ac1e98c9
Greg Roberts <soulpainter@gmail.com>
2018-09-05 21:36:56 -0700

    lib updates

--- enrollment_forecasts/lib/ca_db.py
+++ enrollment_forecasts/lib/ca_db.py
@@ -27,7 +27,7 @@ class CaDb:
     return result
 
   def getSchoolGradeCounts(self, districtId):
-    sql = "SELECT year, sum(kdgn) as 'K', sum(gr_1) as '1', sum(gr_2) as '2', sum(gr_3) as '3', sum(gr_4) as '4', sum(gr_5) as '5', sum(gr_6) as '6', sum(gr_7) as '7', sum(gr_8) as '8', sum(gr_9) as '9', sum(gr_10) as '10', sum(gr_11) as '11', sum(gr_12) as '12' FROM Districts d JOIN Schools s ON s.district_id = d.id JOIN SchoolGradeCounts c ON c.school_id = s.id WHERE d.id =" + str(districtId) + " GROUP BY d.id, year LIMIT 6"
+    sql = "SELECT year, sum(kdgn) as 'K', sum(gr_1) as '1', sum(gr_2) as '2', sum(gr_3) as '3', sum(gr_4) as '4', sum(gr_5) as '5', sum(gr_6) as '6', sum(gr_7) as '7', sum(gr_8) as '8', sum(gr_9) as '9', sum(gr_10) as '10', sum(gr_11) as '11', sum(gr_12) as '12' FROM Districts d JOIN Schools s ON s.district_id = d.id JOIN SchoolGradeCounts c ON c.school_id = s.id WHERE d.id =" + str(districtId) + " GROUP BY d.id, year"
 
     result = self.db.engine.execute(text(sql))
     return result
